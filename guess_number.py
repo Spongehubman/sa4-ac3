2cbe7720dc3da88e771548ea0024e28e0f12fd5b
Nicholas Folis <heffawoozle77@gmail.com>
2024-02-10 20:37:15 -0600

    Added a 'limited guess counter' that counts how many chances a user has left. The program will tell the user how many chances he or she has left. If the user runs out of chances, the game is over.

--- guess_number.py
+++ guess_number.py
@@ -1,11 +1,13 @@
 number = 10
 guess = None
+guesses_left = 3
 
 print("I'm thinking of a number...")
 while exit != True:
-    guess = input("What number am I thinking of? Or press 'q' to quit.: ")
+    if guesses_left > 0:
+        guess = input("What number am I thinking of? Or press 'q' to quit.: ")
 
-    if guess == 'q':
+    if guess == 'q' or guesses_left < 1:
         print(f"The number is {number}.")
         exit = True
     else:
@@ -13,4 +15,10 @@ while exit != True:
             print("Congratulations! You guessed the right number.")
             exit = True
         else:
-            print(f"Incorrect. Please try again!")
+            print(f"Sorry! Incorrect.")
+            guesses_left = guesses_left - 1
+            if guesses_left > 0 and guesses_left > 1:
+                print(f"Please try again! You have {guesses_left} guesses left!")
+            elif guesses_left > 0 and guesses_left == 1:
+                print(f"Please try again! You have {guesses_left} guess left!")
+
